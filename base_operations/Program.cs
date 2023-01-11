9f77e17d66e1feb0bf3b9672c9556ff014dfaf27
Roman <rgvaramadze@bk.ru>
2023-01-11 21:56:14 +0300

    added base

---
+++ base_operations/Program.cs
@@ -0,0 +1,10 @@
+﻿// input / output commands
+
+// Console.WriteLine("Game_Over"); // print a text
+// string x = Console.ReadLine(); // input smth into consolefield
+// Console.WriteLine(x); // then print it in console
+
+// Console.Write("Hey, what's ur favourite profession? Input it: ");
+// string prof = Console.ReadLine( );
+// Console.Write(prof);
+// Console.Write(" is my favourite proffession");
