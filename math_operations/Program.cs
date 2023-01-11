e991b9c8e08d69d9976e21f5d269f162fd27ff0a
Roman <rgvaramadze@bk.ru>
2023-01-11 22:09:38 +0300

    added simple math operations

---
+++ math_operations/Program.cs
@@ -0,0 +1,16 @@
+﻿// Math base-operations in C#
+
+// int a = new Random().Next(1, 19); 
+// int b = new Random().Next(1, 19);
+
+// double a = new Random().Next(1, 19);
+// double b = new Random().Next(1, 19);
+
+// decimal a = new Random().Next(1, 19);
+// decimal b = new Random().Next(1, 19);
+
+Console.WriteLine(a+b);
+Console.WriteLine(a-b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+Console.WriteLine(a%b);
