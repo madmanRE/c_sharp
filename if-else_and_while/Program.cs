aba109e9fd4baf5e31815a5bc4db3b6b78586be4
Roman <rgvaramadze@bk.ru>
2023-01-11 22:37:57 +0300

    added if_else_while

---
+++ if-else_and_while/Program.cs
@@ -0,0 +1,35 @@
+﻿// If / else / while operations
+// Simple programm about a python hucker
+
+Console.WriteLine("Identification...wait...");
+Console.WriteLine("waiting...43%");
+Console.WriteLine("waiting...97%");
+Console.Write("Enter Password: ");
+string password = Console.ReadLine();
+
+if(password.ToLower() != "hardpassword")
+{
+  Console.WriteLine("Yor are hucker! Alarm! Alarm! ");
+}
+else
+{
+  Console.WriteLine("Hellow, dr. John, nice to meet u!");
+}
+
+//when hucker enter the guard_system, he will have a little chance to robe a briliant!
+
+int seconds = 10;
+
+while (seconds > 0)
+{
+  seconds -= 1;
+}
+
+if (1-2 == 3)
+{
+  Console.WriteLine("Catch him!");
+}
+else
+{
+  Console.WriteLine("Python hucker has robbed a laboratory and run away!");
+}
