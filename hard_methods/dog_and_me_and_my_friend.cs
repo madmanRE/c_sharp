c37cfd250bd3bab435cfc865f1109d3afa835a91
Roman <rgvaramadze@bk.ru>
2023-01-11 23:15:53 +0300

    added files

---
+++ hard_methods/dog_and_me_and_my_friend.cs
@@ -0,0 +1,30 @@
+﻿
+// Dog and me and my friend
+
+int count = 0;
+double distance = 10000;
+double firsSpeed = 1;
+double seconSpeed = 2;
+double dogSpeed = 5;
+int flag = 2;
+
+while(distance > 10.0)
+if(flag == 2)
+{
+  double time = distance/(seconSpeed+dogSpeed);
+  flag = 1;
+  distance = distance - (firsSpeed+seconSpeed)*time;
+  count+=1;
+}
+else
+{
+  double time = distance / (firsSpeed + dogSpeed);
+  flag = 2;
+  distance = distance - (firsSpeed + seconSpeed) * time;
+  count+=1;
+  }
+
+
+Console.Write("The dog has run ");
+Console.Write(count);
+Console.Write(" times");
---
+++ recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿// trying recursion - this func can evaluate n(a number) to grade x(a grading-number)
+
+int rec(int n, int x)
+{
+  if (x == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return n * rec(n, x - 1);
+  }
+}
+
+Console.Write(rec(3, 3));
