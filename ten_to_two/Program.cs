5204d53435153ed26a4fa2899628b385bf412e7d
Roman <rgvaramadze@bk.ru>
2023-01-12 10:15:53 +0300

    added files

--- ten_to_two/Program.cs
+++ ten_to_two/Program.cs
@@ -1,13 +1,18 @@
 ﻿// Trying convers 10_number into 2_nimber
+using System.Linq;
 
 int a = 10;
-double q = 0;
+string q = "";
 Console.Write(q);
 
 while(a>0)
 {
-  q += a%2;
+  int x = a%2;
+  q += x.ToString();
   a = a/2;
 }
 
-Console.Write(q);
+
+char[] res = q.ToCharArray();
+Array.Reverse(res);
+Console.WriteLine(res);
