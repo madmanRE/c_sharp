2efdebcc1bc6aa0331f1bde73c9e35ebbf4000e2
Roman <rgvaramadze@bk.ru>
2023-01-13 22:20:38 +0300

    les1

---
+++ seminar/Program.cs
@@ -0,0 +1,72 @@
+﻿//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(a*a);
+
+/*int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+if(a<b)
+{
+  Console.Write("Max: ");
+  Console.Write(b);
+  Console.WriteLine("");
+  Console.Write("Min: ");
+  Console.Write(a);
+}
+else
+{
+  Console.Write("Max: ");
+  Console.Write(a);
+  Console.WriteLine("");
+  Console.Write("Min: ");
+  Console.Write(b);
+}*/
+
+
+/*
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write(max);
+*/
+/*
+int a = new Random().Next(1, 100);
+Console.WriteLine(a);
+
+if(a%2==0)
+{
+  Console.Write("Чет");
+}
+else
+{
+  Console.Write("Нечет");
+}
+*/
+
+
+
+/*
+int a = new Random().Next(10, 54);
+Console.WriteLine(a);
+int x = 1;
+
+while (x <= a)
+{
+  if (x % 2 == 0)
+  {
+    Console.WriteLine(x);
+    x += 2;
+  }
+  else
+  {
+    x += 1;
+  }
+}
+*/
