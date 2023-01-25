ab9fa8352041ede425ec7fa6d1fa7d4c76eebb26
Roman <rgvaramadze@bk.ru>
2023-01-25 15:52:52 +0300

    Updated 4.0

--- seminar/Program.cs
+++ seminar/Program.cs
@@ -465,3 +465,216 @@ if(N%2==0)
   Console.Write(N*N*N);
 }
 */
+
+
+/*
+int [] array = {1,2,3,4,5,6,7,8};
+Console.WriteLine("{0}", string.Join("XXXX", array));
+*/
+
+/*
+int [] array = {3,3,5,6,8,9,2,8,6,4,3,2};
+int count = 0;
+for (int i=0; i < array.Length; i++)
+{
+  if (array[i]%2==0)
+  {
+    count+=array[i];
+  }
+}
+
+Console.WriteLine(count);
+*/
+
+/*
+int[] array = { 6,5,3,7,9,0,4,2,1,5,6,7,89, -9, -12, -22, -112 };
+for (int i = 0; i<array.Length; i++)
+{
+  Console.Write($"___{array[i]*-1}___");
+}
+*/
+
+
+
+//Домашнее задание к 4 семинару.
+
+
+//Задание 1
+//Заполнить массив размером 8 элементов нулями и единицами и вывести на экран.
+
+/*
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(0,2);
+for (int i=0; i < array.Length; i++)
+{
+  Console.Write(array[i]);
+}
+*/
+
+
+//Задание 2
+//Определить, присутствует ли в массиве некоторое число
+
+/*
+int flag = 0;
+int[] array = new int[20];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(0, 33);
+Console.WriteLine("Ваш массив:");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+}
+Console.WriteLine("");
+Console.WriteLine("Введите число, чтобы проверить, содержится ли оно в массиве");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i<array.Length; i++)
+{
+  if (array[i] == n)
+  flag += 1;
+}
+
+if (flag == 0)
+{
+  Console.WriteLine("Числа в массиве нет!");
+}
+else
+{
+  Console.WriteLine($"Ваше число встречается в массиве {flag} раз/а");
+}
+*/
+
+
+//Задание 3. 
+//Задать массив, заполнить случайными 3х значными числами и посчитать кол-во чет/нечет.
+
+/*
+Console.WriteLine("\n");
+Console.WriteLine("Задайте размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(100, 999);
+
+Console.WriteLine("А вот и ваш массивчик!");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+}
+int chet = 0;
+int nexhet = 0;
+
+for (int i = 0; i<array.Length; i++)
+{
+  if (array[i] % 2 == 0) chet += 1;
+  else nexhet += 1;
+}
+Console.WriteLine("\n");
+Console.WriteLine($"Сумма четных чисел в массиве = {chet}\nСумма нечетных = {nexhet}");
+Console.WriteLine("\n");
+*/
+
+//Задание 4.
+//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+
+/*
+int[] array = new int[123];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+array[i] = rand.Next(0, 150);
+int count = 0;
+Console.WriteLine("А вот и ваш массивчик!\n");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+  if (array[i] <= 99 && 10 <= array[i]) count+=1;
+}
+Console.WriteLine("\n");
+Console.WriteLine($"\nКоличество элементов, которые входят в множество [10, 99] равно {count}");
+*/
+
+
+//Задание 5.
+//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+/*
+int[] array = new int[10];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(0, 9);
+int count = 0;
+Console.WriteLine("А вот и ваш массивчик!\n");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+  if (i % 2 != 0) count += array[i];
+}
+Console.WriteLine("\n");
+Console.WriteLine(count);
+*/
+
+
+//Задание 6.
+//Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+/*
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(0, 9);
+int a = 0;
+int b = array.Length-1;
+Console.WriteLine("А вот и ваш массивчик!\n");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+}
+Console.Write("\n");
+if (array.Length % 2 == 0)
+{
+  for (int i = 0; i < array.Length / 2; i++)
+  {
+    Console.WriteLine(array[a] + array[b]);
+    a += 1;
+    b -= 1;
+  }
+}
+else
+{
+  for (int i = 0; i < array.Length / 2; i++)
+  {
+    Console.WriteLine(array[a] + array[b]);
+    a += 1;
+    b -= 1;
+  }
+  Console.WriteLine($"Число посредине (индекс {array.Length/2+1}): {array[a]}");
+}
+*/
+
+//Задание 7.
+//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+//Вещественные числа — это числа, у которых есть дробная часть (она может быть нулевой).
+/*
+double[] array = new double[10];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+  array[i] = rand.Next(-990, 990)/10.0;
+double mi = 9999999999999999.0;
+double ma = -9999999999999999.0;
+Console.WriteLine("А вот и ваш массивчик!\n");
+for (int i = 0; i < array.Length; i++)
+{
+  Console.Write($"_{array[i]}_");
+  if (array[i] < mi) mi = array[i];
+  if (array[i] > ma) ma = array[i];
+}
+Console.WriteLine($"\nРазница числен между максимальным ({ma}) и минимальным элементами массива ({mi}) равна {ma-mi}");
+Console.WriteLine($"\nРазница абсолютных значений между максимальным ({ma}) и минимальным элементами массива ({mi}) равна {Math.Abs(Math.Abs(ma) - Math.Abs(mi))}");
+*/
